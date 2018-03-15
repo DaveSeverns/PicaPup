516d26015f45b7ff4cf6c8b11d8adb66d27ffc1e
daveseverns <david.w.severns@gmail.com>
2018-03-14 23:19:20 -0400

    Firebase connection and dependencies made for User Authentication, RTDB, Cloud Storage, and FCM for notification support in future.

--- app/src/main/java/com/pic_a_pup/dev/pic_a_pup/Controller/HomeFeedActivity.kt
+++ app/src/main/java/com/pic_a_pup/dev/pic_a_pup/Controller/HomeFeedActivity.kt
@@ -3,10 +3,14 @@ package com.pic_a_pup.dev.pic_a_pup.Controller
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.pic_a_pup.dev.pic_a_pup.R
 import kotlinx.android.synthetic.main.activity_home_feed.*
 
 class HomeFeedActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -29,7 +33,14 @@ class HomeFeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_feed)
+        mAuth = FirebaseAuth.getInstance()
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var currentUser = mAuth.currentUser
+        Log.e("CURRENT_USER",currentUser.toString())
     }
 }
--- app/src/main/java/com/pic_a_pup/dev/pic_a_pup/Model/User.kt
+++ app/src/main/java/com/pic_a_pup/dev/pic_a_pup/Model/User.kt
@@ -7,34 +7,12 @@ import java.net.URL
 /**
  * Created by davidseverns on 3/14/18.
  */
-open class User(var userId: String, var name: String, var image: URL?) : Parcelable, Lovable {
+open class User(var userId: String, var name: String, var image: URL?) :  Lovable {
     override fun showDogsSomeLove(doggo: Model.Dog): Model.Dog {
         //TODO needs to change the userVoteCount in a search Object
         return doggo
     }
 
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            TODO("image")) {
-    }
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(userId)
-        parcel.writeString(name)
-    }
 
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User {
-            return User(parcel)
-        }
-
-        override fun newArray(size: Int): Array<User?> {
-            return arrayOfNulls(size)
-        }
-    }
 }
