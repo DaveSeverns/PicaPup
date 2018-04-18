eacf7d7af7a5ca4da0603e38309a5aeba661f379
Derek Witteck <drwitteck@gmail.com>
2018-04-18 12:19:09 -0400

    Map is complete with ability to search for dog parks, stores, and vets.

--- app/src/main/java/com/pic_a_pup/dev/pic_a_pup/Controller/MapsActivity.kt
+++ app/src/main/java/com/pic_a_pup/dev/pic_a_pup/Controller/MapsActivity.kt
@@ -1,28 +1,18 @@
 package com.pic_a_pup.dev.pic_a_pup.Controller
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Location
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.ActivityCompat
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationSettingsRequest
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -30,7 +20,6 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.pic_a_pup.dev.pic_a_pup.R
 import okhttp3.Call
@@ -44,9 +33,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
-    private lateinit var locationCallback: LocationCallback
-    private lateinit var locationRequest: LocationRequest
-    private var locationUpdateState = false
     private lateinit var fabMain: FloatingActionButton
     private lateinit var fabPark: FloatingActionButton
     private lateinit var fabStore: FloatingActionButton
@@ -55,19 +41,60 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     private lateinit var fabClose: Animation
     private var isFabOpen: Boolean = false
     private var placesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    private var searchId: Int? = null
+    private var queryPlaceType: String? = null
+    private var queryPlaceKeyword: String? = null
+    private var queryRadius: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        val fabMain = findViewById<FloatingActionButton>(R.id.fab_main)
-        val fabPark = findViewById<FloatingActionButton>(R.id.fab_park)
-        val fabStore = findViewById<FloatingActionButton>(R.id.fab_store)
-        val fabVet = findViewById<FloatingActionButton>(R.id.fab_vet)
+        fabMain = findViewById(R.id.fab_main)
+        fabPark = findViewById(R.id.fab_park)
+        fabStore = findViewById(R.id.fab_store)
+        fabVet = findViewById(R.id.fab_vet)
 
         fabOpen = AnimationUtils.loadAnimation(applicationContext, R.anim.fab_open)
         fabClose = AnimationUtils.loadAnimation(applicationContext, R.anim.fab_close)
 
+        fabAnimationSetup()
+
+        fabPark.setOnClickListener {
+            searchId = 1
+            queryPlaceType = "park"
+            queryPlaceKeyword = "dog"
+            queryRadius = 16000
+            Toast.makeText(applicationContext, "Searching for Dog Parks", Toast.LENGTH_LONG).show()
+            googlePlacesQuery(searchId!!, queryPlaceType!!, queryPlaceKeyword!!, queryRadius!!)
+        }
+
+        fabStore.setOnClickListener {
+            searchId = 2
+            queryPlaceType = "store"
+            queryPlaceKeyword = "pet"
+            queryRadius = 16000
+            Toast.makeText(applicationContext, "Searching for Pet Stores", Toast.LENGTH_LONG).show()
+            googlePlacesQuery(searchId!!, queryPlaceType!!, queryPlaceKeyword!!, queryRadius!!)
+        }
+
+        fabVet.setOnClickListener {
+            searchId = 3
+            queryPlaceType = "veterinary_care"
+            queryPlaceKeyword = "dog"
+            queryRadius = 16000
+            Toast.makeText(applicationContext, "Searching for Vets", Toast.LENGTH_LONG).show()
+            googlePlacesQuery(searchId!!, queryPlaceType!!, queryPlaceKeyword!!, queryRadius!!)
+        }
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun fabAnimationSetup() {
         fabMain.setOnClickListener {
             if (isFabOpen) {
                 fabPark.startAnimation(fabClose)
@@ -91,63 +118,111 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 isFabOpen = true
             }
         }
-
-        fabPark.setOnClickListener {
-            Toast.makeText(applicationContext, "Searching for parks", Toast.LENGTH_LONG).show()
-            googlePlacesQuery()
-        }
-
-        fabStore.setOnClickListener {
-            Toast.makeText(applicationContext, "Searching for pet stores", Toast.LENGTH_LONG).show()
-        }
-
-        fabVet.setOnClickListener {
-            Toast.makeText(applicationContext, "Searching for Vets", Toast.LENGTH_LONG).show()
-        }
-
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                super.onLocationResult(p0)
-
-                map.clear()
-
-                lastLocation = p0.lastLocation
-                placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude))
-            }
-        }
-
-        createLocationRequest()
     }
 
-    private fun googlePlacesQuery() {
-        val queryPlaceKeyword = "dog"
-        val queryPlaceType = "park"
-        val radius = 16000
+    private fun googlePlacesQuery(id: Int, type: String, keyword: String, radius: Int) {
         var query = "&location=${lastLocation.latitude},${lastLocation.longitude}" +
-            "+&radius=$radius&type=$queryPlaceType&keyword=$queryPlaceKeyword"
+            "+&radius=$radius&type=$type&keyword=$keyword"
         query = "$placesUrl$query&key=$GOOGLE_PLACES_KEY"
 
         val request = Request.Builder().url(query).build()
         val client = OkHttpClient()
-        client.newCall(request).enqueue(object: Callback{
-            override fun onResponse(call: Call?, response: Response?) {
-                val body = response?.body()?.string()
-                println(body)
+        val zoomTo = 11.0f
 
-                val gson = GsonBuilder().create()
-                val data = gson.fromJson(body, Data::class.java)
+        when(id) {
+            1 -> {
+                map.clear()
+                client.newCall(request).enqueue(object: Callback{
+                    override fun onResponse(call: Call?, response: Response?) {
+                        val body = response?.body()?.string()
+                        val gson = GsonBuilder().create()
+                        val data = gson.fromJson(body, Data::class.java)
 
+                        runOnUiThread {
+                            for (i in data.results.indices) {
+                                val latitude = data.results[i].geometry.location.lat
+                                val longitude = data.results[i].geometry.location.lng
+                                val name = data.results[i].name
+                                val address = data.results[i].vicinity
+                                val parkLatLng = LatLng(latitude, longitude)
+
+                                placeMarkerOnMap(parkLatLng, name, address)
+                                map.animateCamera(CameraUpdateFactory.zoomTo(zoomTo))
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        println("Failed")
+                    }
+                })
             }
-            override fun onFailure(call: Call?, e: IOException?) {
-                println("Failed")
+            2 -> {
+                map.clear()
+                client.newCall(request).enqueue(object: Callback{
+                    override fun onResponse(call: Call?, response: Response?) {
+                        val body = response?.body()?.string()
+                        val gson = GsonBuilder().create()
+                        val data = gson.fromJson(body, Data::class.java)
+
+                        runOnUiThread {
+                            for (i in data.results.indices) {
+                                val latitude = data.results[i].geometry.location.lat
+                                val longitude = data.results[i].geometry.location.lng
+                                val name = data.results[i].name
+                                val address = data.results[i].vicinity
+                                val parkLatLng = LatLng(latitude, longitude)
+
+
+                                placeMarkerOnMap(parkLatLng, name, address)
+                                map.animateCamera(CameraUpdateFactory.zoomTo(zoomTo))
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        println("Failed")
+                    }
+                })
             }
-        })
+            3 -> {
+                map.clear()
+                client.newCall(request).enqueue(object: Callback{
+                    override fun onResponse(call: Call?, response: Response?) {
+                        val body = response?.body()?.string()
+                        val gson = GsonBuilder().create()
+                        val data = gson.fromJson(body, Data::class.java)
+
+                        runOnUiThread {
+                            for (i in data.results.indices) {
+                                val latitude = data.results[i].geometry.location.lat
+                                val longitude = data.results[i].geometry.location.lng
+                                val name = data.results[i].name
+                                val address = data.results[i].vicinity
+                                val parkLatLng = LatLng(latitude, longitude)
+
+                                placeMarkerOnMap(parkLatLng, name, address)
+                                map.animateCamera(CameraUpdateFactory.zoomTo(zoomTo))
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        println("Failed")
+                    }
+                })
+            }
+        }
+
+    }
+
+    private fun placeMarkerOnMap(location: LatLng, name: String, address: String) {
+        val markerOptions = MarkerOptions()
+            .position(location)
+            .title(name)
+            .snippet(address)
+
+        map.addMarker(markerOptions)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -177,83 +252,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 lastLocation = location
                 val currentLatLng = LatLng(location.latitude, location.longitude)
 
-                placeMarkerOnMap(currentLatLng)
+//                placeMarkerOnMap(currentLatLng)
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 16.0f))
             }
         }
     }
 
-    private fun placeMarkerOnMap(location: LatLng) {
-        val markerOptions = MarkerOptions().position(location)
-
-        map.addMarker(markerOptions)
-    }
-
-    private fun locationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE)
-            return
-        }
-
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
-    }
-
-    @SuppressLint("RestrictedApi")
-    private fun createLocationRequest() {
-        locationRequest = LocationRequest()
-
-        locationRequest.interval = 10000
-        locationRequest.fastestInterval = 5000
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-
-        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
-        val client = LocationServices.getSettingsClient(this)
-        val task = client.checkLocationSettings(builder.build())
-
-        task.addOnSuccessListener {
-            locationUpdateState = true
-            locationUpdates()
-        }
-        task.addOnFailureListener {e ->
-            if(e is ResolvableApiException)
-                try{
-                    e.startResolutionForResult(this@MapsActivity, REQUEST_CHECK_SETTINGS)
-                } catch (sendEx: IntentSender.SendIntentException) {
-
-                }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_CHECK_SETTINGS) {
-            if(resultCode == Activity.RESULT_OK) {
-                locationUpdateState = true
-                locationUpdates()
-            }
-        }
-//        if(requestCode == PLACES_PICKER_REQUEST) {
-//            val place = PlacePicker.getPlace(this, data)
-//            var addressText = place.name.toString()
-//            addressText += "\n" + place.address.toString()
-//
-//            placeMarkerOnMap(place.latLng)
-//        }
-    }
-
     override fun onPause() {
         super.onPause()
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 
     override fun onResume() {
         super.onResume()
-        if(!locationUpdateState) {
-            locationUpdates()
-        }
     }
 
     companion object {
@@ -261,11 +271,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             return Intent(context, MapsActivity::class.java)
         }
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
-        private const val REQUEST_CHECK_SETTINGS = 2
         private const val GOOGLE_PLACES_KEY = "AIzaSyAxtwhf8egj3eThPnZHIr8HwWcqbd80FuQ"
     }
 }
 
+//Data classes for Google Places API search
 class Data(val results: List<Result>)
 class Result(val name: String, val vicinity: String, val geometry: Geometry)
 class Geometry(var location: Locations)
