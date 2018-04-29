c64f6804f76082b32dad1f4c6db30ab1cc30f278
daveseverns <david.w.severns@gmail.com>
2018-04-29 13:30:31 -0400

    notifiction being built

--- app/src/main/java/com/pic_a_pup/dev/pic_a_pup/FirebaseServices/PaPFirebaseMessagingService.kt
+++ app/src/main/java/com/pic_a_pup/dev/pic_a_pup/FirebaseServices/PaPFirebaseMessagingService.kt
@@ -10,7 +10,9 @@ import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.media.RingtoneManager
+import com.pic_a_pup.dev.pic_a_pup.Controller.MapsActivity
 import com.pic_a_pup.dev.pic_a_pup.R
 import java.util.*
 
@@ -20,7 +22,20 @@ class PaPFirebaseMessagingService :  FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         Log.e("MESSAGE FROM", remoteMessage!!.from)
         if((remoteMessage?.data!!.isNotEmpty()) && (remoteMessage.notification != null)){
+
+            val lat = remoteMessage.data.get("locationLat")
+            val lon = remoteMessage.data.get("locationLon")
+            val intent = Intent(applicationContext, MapsActivity::class.java)
+            intent.putExtra("lat",lat)
+            intent.putExtra("lon", lon)
+            val pendingIntent = PendingIntent.getActivity(applicationContext, 6969,intent,0)
             Log.e("Message Payload", remoteMessage.data.toString())
+            var notification = Notification.Builder(applicationContext).setContentTitle(remoteMessage.notification!!.body)
+                    .setContentText(remoteMessage.data.toString()).setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentIntent(pendingIntent).build()
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(1791, notification)
 
         }
 
