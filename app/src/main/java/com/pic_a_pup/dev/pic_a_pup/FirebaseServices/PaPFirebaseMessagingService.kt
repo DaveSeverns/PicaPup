25336aa5dbf043eb8a24f24af0b4a1869cf1b43c
daveseverns <david.w.severns@gmail.com>
2018-04-29 13:13:40 -0400

    getting the notification from the cloud function

--- app/src/main/java/com/pic_a_pup/dev/pic_a_pup/FirebaseServices/PaPFirebaseMessagingService.kt
+++ app/src/main/java/com/pic_a_pup/dev/pic_a_pup/FirebaseServices/PaPFirebaseMessagingService.kt
@@ -1,24 +1,30 @@
 package com.pic_a_pup.dev.pic_a_pup.FirebaseServices
 
+import android.app.Notification
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import android.app.NotificationManager
+import android.media.RingtoneManager
+import com.pic_a_pup.dev.pic_a_pup.R
+import java.util.*
+
 
 class PaPFirebaseMessagingService :  FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-
         Log.e("MESSAGE FROM", remoteMessage!!.from)
-        if(remoteMessage?.data!!.isNotEmpty()){
+        if((remoteMessage?.data!!.isNotEmpty()) && (remoteMessage.notification != null)){
             Log.e("Message Payload", remoteMessage.data.toString())
+
         }
 
-        if(remoteMessage.notification != null){
-            Log.e("Notification Body ", remoteMessage.notification!!.body)
-        }
+
         //super.onMessageReceived(remoteMessage)
     }
 }
